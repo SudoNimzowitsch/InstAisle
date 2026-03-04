fb9d5f1270180262a9c617937ffd7f5d48078080
SudoNimzowitsch <42681152+SudoNimzowitsch@users.noreply.github.com>
2026-03-04 17:22:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // InstAisle Service Worker
 // Caches the app shell for offline use and fast loading
 
-const CACHE_NAME = 'instaisle-v5';
+const CACHE_NAME = 'instaisle-v6';
 
 // Core files to cache on install
 const PRECACHE_URLS = [
