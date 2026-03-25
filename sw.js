191c27d0f7d458550988c5a3228e4de5bfff5b91
SudoNimzowitsch <gabe.herczeg@gmail.com>
2026-03-24 21:02:58 -0400

    fix: move tiles out of nested subdirectory

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // InstAisle Service Worker
 // Caches the app shell for offline use and fast loading
 
-const CACHE_NAME = 'instaisle-v204';
+const CACHE_NAME = 'instaisle-v205';
 
 // Core files to cache on install
 const PRECACHE_URLS = [
